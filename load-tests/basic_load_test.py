3ad107893af5c42c3541ff868af77c6d7b24b86a
Chris Lloyd <chris.lloyd@rezare.co.nz>
2019-02-20 14:40:55 +1300

    Change to TaskSequence
    Make things a bit more complicated, do a few more requests.

--- load-tests/basic_load_test.py
+++ load-tests/basic_load_test.py
@@ -1,13 +1,24 @@
-from locust import HttpLocust, TaskSet, task
+from locust import HttpLocust, TaskSet, TaskSequence, task, seq_task
 
-class UserBehavior(TaskSet):
-    @task
+class UserBehavior(TaskSequence):
+    @seq_task(1)
     def get_home(self):
       print("Getting Home Page...")
       self.locust.client.get("/")
 
+    @seq_task(2)
+    def get_about(self):
+      print("Getting About Page...")
+      self.locust.client.get("/about")
+
+    @seq_task(3)
+    @task(10)
+    def get_error(self):
+      print("Getting Error Page...")
+      self.locust.client.get("/error")
+
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
-    min_wait = 2000
-    max_wait = 5000
+    min_wait = 500
+    max_wait = 1500
